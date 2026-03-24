b35d3d63b6f0b8c3a1a90ffb707c4855fd7eacde
Mohd Arbaaz <mohd.arbaaz.0503@gmail.com>
2026-03-24 16:13:00 +0530

    fix(cors): allow x-company-context header

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'x-company-context']
 }));
 
 app.use(express.json());
